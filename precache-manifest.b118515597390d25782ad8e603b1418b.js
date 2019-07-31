96f936c4de800f3c835cdb9fef664e366e60a421
Madan <madan@noesyssoftware.com>
2019-07-31 15:53:56 +0530

    Updates

---
+++ precache-manifest.b118515597390d25782ad8e603b1418b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e98bf101bf158c56bc06c72832c09bee",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "6bc523bbe08dfab9b3ee",
+    "url": "/myportfolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/myportfolio/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "6bc523bbe08dfab9b3ee",
+    "url": "/myportfolio/static/js/main.638b9a1d.chunk.js"
+  },
+  {
+    "revision": "314136164c1c0c643422",
+    "url": "/myportfolio/static/js/runtime~main.69c47bdd.js"
+  }
+]);
